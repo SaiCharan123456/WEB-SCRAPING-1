671cea231170487f5a5a60a9c3fa4b02024aee3e
SaiCharan123456 <77579506+SaiCharan123456@users.noreply.github.com>
2022-02-18 20:05:25 +0530

    Add files via upload

--- WEB SCRAPING 1.py
+++ WEB SCRAPING 1.py
@@ -7,25 +7,35 @@ browser = webdriver.Chrome("chromedriver.exe")
 browser.get(START_URL)
 time.sleep(10)
 def scrape():
-    headers = ["V Mag. (mV)","	Proper name","	Bayer designation","	Distance (ly)","	Spectral class","	Mass (M☉)","	Radius (R☉)"	,"Luminosity (L☉)"]
+    head = ["V Mag. (mV)","Proper name","Bayer designation","Distance (ly)","Spectral class","Mass (M☉)","Radius (R☉)"	,"Luminosity (L☉)"]
     planet_data = []
-    for i in range(0, 4):
-        soup = BeautifulSoup(browser.page_source, "html.parser")
-        for tr_tag in soup.find_all("tr", attrs={"class", "exoplanet"}):
+
+    soup = BeautifulSoup(browser.page_source, "html.parser")
+    for tr_tag in soup.find_all("tr"):
             td_tags = tr_tag.find_all("td")
             temp_list = []
+            print("before a tag")
             for index, td_tag in enumerate(td_tags):
                 if index == 1:
-                    temp_list.append(td_tag.find_all("a")[0].contents[0])
+                    print("in the a tag")
+                    a_tag = td_tag.find_all("a")
+                    temp_list.append(a_tag)
                 else:
                     try:
                         temp_list.append(td_tag.contents[0])
                     except:
                         temp_list.append("")
             planet_data.append(temp_list)
-        browser.find_element_by_xpath('//*[@id="mw-content-text"]/div[1]/table/thead/tr/th[8]').click()
+            print("after creating list")
+            print(head)
+            
     with open("web scrapper.csv", "w") as f:
         csvwriter = csv.writer(f)
-        csvwriter.writerow(headers)
+        csvwriter.writerow(head)
         csvwriter.writerows(planet_data)
+    
+        
 scrape()
+#head = ["V Mag. (mV)","Proper name","Bayer designation","Distance (ly)","Spectral class","Mass (M☉)","Radius (R☉)"	,"Luminosity (L☉)"]
+#planet = 
+
